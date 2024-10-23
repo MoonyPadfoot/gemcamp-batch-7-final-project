bdf4f4def65911cd2bc69574e311666820c65aaa
Renier Antonio <eurekavixen@gmail.com>
2024-10-23 16:25:40 +0800

    [Feature 1] added: devise and created user columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_23_074002) do
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "username"
+    t.integer "role", default: 0
+    t.string "phone_number"
+    t.integer "coins"
+    t.decimal "total_deposit", precision: 10
+    t.integer "children_members"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
 end
