24aa69dcd781b1ef563beded1addf233abbfda5c
Renier Antonio <eurekavixen@gmail.com>
2024-10-25 17:26:24 +0800

    [Feature 1] modified: separated login routes for client and admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :client, class_name: 'User', only: [:sessions], controllers: {
+    sessions: 'client/sessions'
+  }
+  
+  devise_for :admin, class_name: 'User', only: [:sessions], controllers: {
+    sessions: 'admin/sessions'
+  }
 
   constraints(ClientDomainConstraint.new) do
     namespace :admin do
