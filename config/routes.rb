b69a66596d611cb5221076de7ac74e185f5dd98e
Renier Antonio <eurekavixen@gmail.com>
2024-10-25 18:02:59 +0800

    [Feature 2] modified: constrained devise sign in with domain

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,21 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  devise_for :client, class_name: 'User', only: [:sessions], controllers: {
-    sessions: 'client/sessions'
-  }, path_names: { sign_in: 'sign_in' }, path: ''
-
-  devise_for :admin, class_name: 'User', only: [:sessions], controllers: {
-    sessions: 'admin/sessions'
-  }, path_names: { sign_in: 'sign_in' }, path: ''
 
   constraints(ClientDomainConstraint.new) do
+    devise_for :client, class_name: 'User', only: [:sessions], controllers: {
+      sessions: 'client/sessions'
+    }, path_names: { sign_in: 'sign_in' }, path: ''
+
     namespace :admin do
       resources :home, only: [:index]
     end
   end
 
   constraints(AdminDomainConstraint.new) do
+    devise_for :admin, class_name: 'User', only: [:sessions], controllers: {
+      sessions: 'admin/sessions'
+    }, path_names: { sign_in: 'sign_in' }, path: ''
+
     namespace :client do
       resources :home, only: [:index]
     end
