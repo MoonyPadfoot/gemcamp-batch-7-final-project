a81e33cbc69949059838cc77bb6f290de772838a
Renier Antonio <eurekavixen@gmail.com>
2024-10-29 10:03:19 +0800

    [Feature 1] added: initial fields validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,15 @@ class User < ApplicationRecord
 
   enum role: { client: 0, admin: 1 }
 
+  validates :username, presence: true, uniqueness: true
   validates :phone_number, phone: {
     possible: true,
     allow_blank: true,
     types: %i[voip mobile],
     countries: [:ph]
   }
+  validates :coins, numericality: { only_integer: true, greater_than: 0 }
+  validates :total_deposit, numericality: { greater_than: -1 }
+  validates :children_members, numericality: { greater_than: -1 }
+
 end
