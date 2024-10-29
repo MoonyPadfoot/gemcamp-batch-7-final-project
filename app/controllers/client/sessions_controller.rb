272a416aca17c84cebc6c5149a6519917d6aaccd
Renier Antonio <eurekavixen@gmail.com>
2024-10-29 16:01:05 +0800

    [Refactor] modified: clean up devise session and client controllers

--- app/controllers/client/sessions_controller.rb
+++ app/controllers/client/sessions_controller.rb
@@ -1,17 +1,3 @@
 class Client::SessionsController < Devise::SessionsController
-  protected
 
-  def after_sign_in_path_for(resource)
-    stored_location_for(resource) || custom_redirect_path
-  end
-
-  private
-
-  def custom_redirect_path
-    if current_client.admin?
-      admin_root_path
-    else
-      client_root_path
-    end
-  end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,19 +2,4 @@
 
 class Users::SessionsController < Devise::SessionsController
 
-  protected
-
-  def after_sign_in_path_for(resource)
-    stored_location_for(resource) || custom_redirect_path
-  end
-
-  private
-
-  def custom_redirect_path
-    if current_user.admin?
-      admin_root_path
-    else
-      client_root_path
-    end
-  end
 end
