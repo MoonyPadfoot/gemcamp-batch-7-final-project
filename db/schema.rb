f8f1a29457efaff2bc8fff4228f83ab1df1fb15f
Renier Antonio <eurekavixen@gmail.com>
2024-10-29 15:57:59 +0800

    modified: schema version change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_29_035852) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_29_040246) do
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "username"
