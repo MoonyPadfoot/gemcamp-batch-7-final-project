2dd07781a3d9c3b9639405a0843ef6bc74b77049
Renier Antonio <eurekavixen@gmail.com>
2024-10-30 19:53:51 +0800

    [Feature 1] modified: removed redundant code

--- app/controllers/client/home_controller.rb
+++ app/controllers/client/home_controller.rb
@@ -2,17 +2,4 @@ class Client::HomeController < ClientsController
   def index #refactor
     render template: 'client/home/index'
   end
-
-  private
-
-  def authorize_client
-    if current_user&.admin?
-      sign_out(current_admin)
-      redirect_to new_client_session_path, alert: 'You are not allowed to access this part of the site'
-    end
-  end
-
-  def current_user
-    warden.authenticate(scope: :client)
-  end
 end
