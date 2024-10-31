fc96d4b142facfa59cdd992d92ba7d67c8fd5279
Renier Antonio <eurekavixen@gmail.com>
2024-10-31 16:36:57 +0800

    [Feature 1] modified: validation for username and phone to allow null and unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,24 +9,15 @@ class User < ApplicationRecord
 
   has_many :addresses, class_name: "Client::Address"
 
-  validates :username, presence: true, uniqueness: true
+  validates :username, uniqueness: true, allow_nil: true
   validates :phone_number, phone: {
     possible: true,
     allow_blank: true,
     types: %i[voip mobile],
     countries: [:ph]
   }
-  validates :phone_number, uniqueness: { case_sensitive: false }
   validates :coins, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :total_deposit, presence: true
   validates :children_members, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :image, allow_blank: true, format: { with: %r{.(gif|jpg|png)\Z}i, message: 'must be a URL for GIF, JPG or PNG image.' }
-
-  before_save :normalize_phone_number
-
-  private
-
-  def normalize_phone_number
-    self.phone_number = Phonelib.parse(phone_number).e164
-  end
 end
