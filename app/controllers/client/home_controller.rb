d78025fbf364f614891d357887faf1724394d255
Renier Antonio <eurekavixen@gmail.com>
2024-11-04 13:05:59 +0800

    [Feature 1] fixed: allowing admin to login in client domain

--- app/controllers/client/home_controller.rb
+++ app/controllers/client/home_controller.rb
@@ -1,4 +1,23 @@
-class Client::HomeController < ClientsController
+class Client::HomeController < ApplicationController
+  before_action :authorize_client
+
   def index
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_client_session_path
+  end
+
+  private
+
+  def authorize_client
+    if current_user&.admin?
+      sign_out(current_admin)
+      redirect_to new_client_session_path, alert: 'You are not allowed to access this part of the site'
+    end
+  end
+
+  def current_user
+    warden.authenticate(scope: :client)
   end
 end
