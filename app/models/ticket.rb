add45d0eaa86621e587dc98bda96ecda714a23fb
Renier Antonio <eurekavixen@gmail.com>
2024-11-08 10:17:58 +0800

    [Feature 15] added: callback after ticket save

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,8 @@
 class Ticket < ApplicationRecord
   include AASM
 
+  after_save :deduct_user_coin
+
   belongs_to :user
   belongs_to :item, class_name: 'Admin::Item'
 
@@ -21,10 +23,6 @@ class Ticket < ApplicationRecord
     end
   end
 
-  def deduct_coin
-    user.update(coins: user.coins - 1)
-  end
-
   def refund_coin
     user.update(coins: user.coins + 1)
   end
@@ -36,4 +34,7 @@ class Ticket < ApplicationRecord
     self.update(serial_number: "#{Time.current.strftime("%Y%m%d")}-#{item.id}-#{item.batch_count}-#{number_count.to_s.rjust(4, '0')}")
   end
 
+  def deduct_user_coin
+    user.update(coins: user.coins - 1)
+  end
 end
