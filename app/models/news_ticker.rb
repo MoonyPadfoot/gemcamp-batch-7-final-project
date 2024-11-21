2aa720cca85ad7e387594f01698144b324638437
Renier Antonio <eurekavixen@gmail.com>
2024-11-21 12:37:24 +0800

    [Feature 30] modified: removed optional in admin relationship and modified edit view h1 tag

--- app/models/news_ticker.rb
+++ app/models/news_ticker.rb
@@ -1,7 +1,7 @@
 class NewsTicker < ApplicationRecord
   enum status: { inactive: 0, active: 1 }
 
-  belongs_to :admin, class_name: User.name, foreign_key: 'admin_id', optional: true
+  belongs_to :admin, class_name: User.name, foreign_key: 'admin_id'
 
   validates :content, presence: true
 
