583ac54668981df5198949f7d7113205520eb3e9
Renier Antonio <eurekavixen@gmail.com>
2024-11-22 16:27:13 +0800

    [Feature 29] added: invite list filter

--- app/controllers/admin/invites_controller.rb
+++ app/controllers/admin/invites_controller.rb
@@ -1,7 +1,8 @@
 class Admin::InvitesController < AdminsController
   def index
     @clients = User.includes(:parent).where(role: :client)
-                   .where.not(parent: { id: nil })
-                   .page(params[:page]).per(10)
+    @clients = @clients.where.not(parent: { id: nil })
+    @clients = @clients.where(parent: { email: params[:email] }) if params[:email].present?
+    @clients = @clients.page(params[:page]).per(10)
   end
 end
