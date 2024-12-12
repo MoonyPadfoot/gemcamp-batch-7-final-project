d679bfd5f9eb75a239480315c174d9de52822f13
Renier Antonio <eurekavixen@gmail.com>
2024-12-12 20:15:36 +0800

    [Refactor]: paid event on guard fail shows error messages

--- app/controllers/admin/users/orders/deduct_controller.rb
+++ app/controllers/admin/users/orders/deduct_controller.rb
@@ -10,12 +10,6 @@ class Admin::Users::Orders::DeductController < AdminsController
     @order = Order.new(params.require(:order).permit(:coin, :genre, :remarks))
     @order.user = User.client.find(params[:client_id])
 
-    if @order.user.coins == 0
-      flash[:alert] = "Coin insufficient and can't be deducted"
-      render template: "admin/users/orders/new", status: :unprocessable_entity
-      return
-    end
-
     ActiveRecord::Base.transaction do
       if @order.valid?(:balance_operate) && @order.save
         order_pay
@@ -38,7 +32,9 @@ class Admin::Users::Orders::DeductController < AdminsController
       if @order.may_cancel?
         @order.cancel!
       end
-      flash[:alert] = "Coin failed to deduct: #{ @order.errors.full_messages.to_sentence }"
+      flash[:alert] = @order.errors.present? ?
+                        "Coin failed to deduct: #{ @order.errors.full_messages.to_sentence }" :
+                        "Coin failed to deduct: User coin is insufficient to deduct."
       render template: "admin/users/orders/new", status: :unprocessable_entity
       raise ActiveRecord::Rollback
     end
